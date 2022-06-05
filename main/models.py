40367011cd53b10ad236f897b351d8cc8e59d643
yukabedAB <adragon60000@gmail.com>
2022-06-05 02:28:42 -0700

    test

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from argparse import ONE_OR_MORE
 from django.db import models
 
 # Create your models here.
@@ -14,7 +15,36 @@ class lesson(models.Model):
 
     def __str__(self):
         pass
-   
+
+
+class news(models.Model):
+   from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class news(models.Model):
+
+    STATUS_CHOICES =('draft', 'Draft'),('published', 'Published')
+
+
+    title = models.CharField(max_length=450)
+    slug = models.SlugField(max_length=350,unique_for_date='publish')
+
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='main_news')
+
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
 
 
 
